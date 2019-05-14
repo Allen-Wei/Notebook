6c6419bc6a2813390a2796d43016c6ff06edcd2c
Alan <alan.wei@live.com>
2019-05-14 13:13:46 +0800

    增加引用地址

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
+#ref: https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
 RED='\033[0;31m'
 NC='\033[0m' 
 
--- repo.init.sh
+++ repo.init.sh
@@ -1,3 +1,4 @@
+#ref: https://stackoverflow.com/questions/17291995/push-existing-project-into-github
 #git clone https://gitee.com/alanway/Notebook.git
 git remote add gitlab https://gitlab.com/alanwei/notebook.git
 git remote add github https://github.com/Allen-Wei/Notebook.git
