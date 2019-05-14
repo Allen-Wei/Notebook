9114a8f1335730d4845eba574f87d4e1d547f74c
Alan <alan.wei@live.com>
2019-05-14 13:12:07 +0800

    支持参数

--- push.sh
+++ push.sh
@@ -1,5 +1,13 @@
+RED='\033[0;31m'
+NC='\033[0m' 
+
 git add --all
-git commit -a -m "add"
+git commit -a -m $1
+echo -e "${RED}push to origin${NC}"
+git push origin master
+echo -e "${RED}push to gitlab${NC}"
 git push gitlab master
+echo -e "${RED}push to github${NC}"
 git push github master
+echo -e "${RED}push to oschina${NC}"
 git push oschina master
