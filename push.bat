e0c678b6e92be6c4bb93b3bfd4a79b1e4345a46a
alan <alan.wei@live.com>
2020-08-22 18:13:47 +0800

    更新脚本

--- push.bat
+++ push.bat
@@ -1,1 +1,1 @@
-git pull && git add --all && git commit -a -m %1 && git push && git push github master && git push gitlab master && git push oschina master
+git push && git push github master && git push gitlab master && git push oschina master
